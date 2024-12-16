eb0d69fbfdd8545181c4a49bbf3337eaf866f052
Raul Guilherme <raulnicofabi@gmail.com>
2024-12-16 16:04:47 -0300

    Update login.js

--- login.js
+++ login.js
@@ -9,3 +9,17 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+document.getElementById('signin-form').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    const email = document.getElementById('login-email').value;
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        alert("Por favor, insira um e-mail válido.");
+        return;
+    }
+
+    window.location.href = 'index.html';
+});
